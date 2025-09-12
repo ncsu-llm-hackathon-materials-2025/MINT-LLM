00b922e61cbe8ce8bd9443999a63aa03f2384ce3
nisargjoshi10 <nisargj@uw.edu>
2025-09-12 11:37:37 -0400

    adding custom RMSD function with MDAnalyis

--- project/api/analysis.py
+++ project/api/analysis.py
@@ -2,6 +2,7 @@ import MDAnalysis as mda
 from MDAnalysis.analysis import rms
 
 def run_rmsd(topology_file, trajectory_file):
+    print("using the provided RMSD function")
     u = mda.Universe(topology_file, trajectory_file)
     R = rms.RMSD(u, u, select="backbone").run()
     return R.rmsd.tolist()
